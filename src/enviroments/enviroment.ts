270fde4c260b7885ee4623a615dc7e60981e7de1
jhonsmall <linuxdevsteam@gmail.com>
2017-11-10 07:38:59 -0500

    Listo Enviroments

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyAGJBGx29Vjo6bTOOktblIpbqJVleOnb5s",
+      authDomain: "auditoresycontadores-36ba5.firebaseapp.com",
+      databaseURL: "https://auditoresycontadores-36ba5.firebaseio.com",
+      projectId: "auditoresycontadores-36ba5",
+      storageBucket: "auditoresycontadores-36ba5.appspot.com",
+      messagingSenderId: "736097897099"
+    },
+    stripe: "pk_test_R9pB1rfVmEkBUqrJmv5rUvnH"
+  };
